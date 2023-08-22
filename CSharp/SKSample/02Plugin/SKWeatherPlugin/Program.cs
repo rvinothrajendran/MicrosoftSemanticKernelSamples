f444d50f2a505e7034260ec41edd83f9532718ed
Vinoth Rajendran <r.vinoth@live.com>
2023-08-22 11:05:50 +0200

    Refacor the sample code

---
+++ CSharp/SKSample/02Plugin/SKWeatherPlugin/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConfigSettings;
+using Microsoft.SemanticKernel;
+using SKWeatherPlugin.Plugins.Weather;
+
+namespace SKWeatherPlugin
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Microsoft Semantic Kernel Plugin");
+
+            var kernel = new KernelBuilder().Build();
+
+            var weatherPlugin = kernel.ImportSkill(new WeatherPlugin(Settings.WeatherApiKey), nameof(WeatherPlugin));
+
+            var report = await weatherPlugin["GetWeatherAsync"].InvokeAsync("chennai");
+
+            Console.WriteLine(report);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp/SKSample/Settings/ConfigSettings/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace ConfigSettings;
+
+public static class Settings
+{
+    public static string ApiKey = "";
+    public static string Endpoint = "";
+    public static string DeploymentName = "";
+    public static string WeatherApiKey { get; } = "";
+}
