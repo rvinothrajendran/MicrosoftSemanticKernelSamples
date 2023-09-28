fa8eb3427b21a972bae116d360c38b4d1c3d06b8
Vinoth Rajendran <r.vinoth@live.com>
2023-09-28 09:06:25 +0200

    Update ConfigParameters.cs

--- CSharp/SKSample/Settings/ConfigSettings/ConfigParameters.cs
+++ CSharp/SKSample/Settings/ConfigSettings/ConfigParameters.cs
@@ -2,17 +2,17 @@
 
 public static class ConfigParameters
 {
-    public static string ApiKey { get; } = "d6d8226371a84c0faa50de24b237ec57";
-    public static string Endpoint { get; } = "https://botgpttest.openai.azure.com/";
+    public static string ApiKey { get; } = "";
+    public static string Endpoint { get; } = "";
     public static string DeploymentOrModelId { get; } = "text-davinci-003";
 
     public static string EndpointType { get; } = "text-completion";
     public static string ServiceType { get; } = "AZUREOPENAI";
 
-    public static string WeatherApiKey { get; } = "a72328bf37064cc6b9a81119232707";
+    public static string WeatherApiKey { get; } = "";
 
 
-    public static string SpeechKey = "6b0bb56da60b4090b9a5f99d9c300e79";
-    public static string SpeechRegion = "eastus";
+    public static string SpeechKey = "";
+    public static string SpeechRegion = "";
 
 }
