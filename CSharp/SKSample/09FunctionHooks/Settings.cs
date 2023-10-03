19a2d311153da0ddb04b0a669f5ad2ef110fd594
Vinoth <r.vinoth@live.com>
2023-10-03 19:07:40 +0200

    Created sample for how to use SK pre and post hooks

---
+++ CSharp/SKSample/09FunctionHooks/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionHooks
+{
+    public static class Settings
+    {
+        public static string Endpoint => "";
+        public static string ApiKey => "";
+        public static string DeploymentOrModelId => "";
+        public static string WeatherApiKey { get; } = "";
+    }
+}
