c6004dffaefb308732aec3f137c5935c5a77e9a2
Vinoth <r.vinoth@live.com>
2023-12-25 12:13:18 +0100

    Added Prompt Function and YAML demos

---
+++ SKSampleCSharp/PromptFunction/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace PromptFunctionDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, Microsoft Semantic Kernel - 1.0 - Prompt Functions");
+
+            Console.WriteLine("Enter the text with city information");
+
+            var userInput = Console.ReadLine();
+
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var kernel = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(Config.DeploymentOrModelId, Config.Endpoint, Config.ApiKey)
+                .Build();
+
+            //Import Prompt Functions
+
+            var plugInDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins","location");
+            kernel.ImportPluginFromPromptDirectory(plugInDirectory, "location");
+
+            //Get the function from location plugin
+            var cityKernelFunction = kernel.Plugins.GetFunction("location", "city");
+
+            //Kernel arguments
+
+            var kernelArguments = new KernelArguments()
+            {
+                {
+                    "input", userInput
+                }
+            };
+
+
+            var functionResults = await kernel.InvokeAsync(cityKernelFunction, kernelArguments);
+
+            Console.WriteLine(functionResults.GetValue<string>());
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ SKSampleCSharp/PromptYAMLDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace PromptYAMLDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, Microsoft Semantic Kernel - 1.0 - YAML Config");
+
+            Console.WriteLine("Enter the text with city name");
+
+            var userInput = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var kernelBuilder = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(Config.DeploymentOrModelId, Config.Endpoint, Config.ApiKey)
+                .Build();
+
+
+            //Read the YAML config file
+            var yamlDirectory = Path.Combine(Directory.GetCurrentDirectory(),"Plugins","Yaml","City.yaml");
+
+            var yamlPromptConfig = await File.ReadAllTextAsync(yamlDirectory);
+
+            //Create a SK function from the YAML config
+            var yamlfunction = kernelBuilder.CreateFunctionFromPromptYaml(yamlPromptConfig);
+
+            //Create Kernel arguments
+            var kernelArguments = new KernelArguments()
+            {
+                {
+                    "input", userInput
+                }
+            };
+
+            //Run the SK function or Invoke the SK function
+
+            var functionResult = await kernelBuilder.InvokeAsync(yamlfunction, kernelArguments);
+
+            Console.WriteLine(functionResult.GetValue<string>());
+
+            Console.Read();
+
+        }
+    }
+}
