77e656c355dea1a9011de33e513a6782a826053d
Vinoth <r.vinoth@live.com>
2023-12-27 11:39:42 +0100

    Added Streaming API

---
+++ SKSampleCSharp/StreamingDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace StreamingDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, Microsoft Semantic Kernel - 1.0 - Kernel Streaming");
+
+            Console.WriteLine("Enter the text with city name");
+
+            var userInput = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var kernelBuilder = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(Config.DeploymentOrModelId, Config.Endpoint, Config.ApiKey)
+                .Build();
+
+
+            //Read the YAML config file
+            var yamlDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins", "Yaml", "City.yaml");
+
+            var yamlPromptConfig = await File.ReadAllTextAsync(yamlDirectory);
+
+            //Create SK function from the YAML config
+            var yamlfunction = kernelBuilder.CreateFunctionFromPromptYaml(yamlPromptConfig);
+
+            //Create Kernel arguments
+            var kernelArguments = new KernelArguments()
+            {
+
+                {
+                    "input", userInput
+                }
+            };
+
+            //Run the SK function or Invoke the SK function
+
+            await foreach (var result in kernelBuilder.InvokeStreamingAsync(yamlfunction, kernelArguments))
+            {
+                Console.Write(result);
+                Thread.Sleep(100);
+            }
+
+            Console.Read();
+        }
+    }
+}
