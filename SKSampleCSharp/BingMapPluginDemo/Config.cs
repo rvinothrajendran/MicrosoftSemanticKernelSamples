73ecdec6f9d29fc5f4ac3ce0a0dfd62d61923ea2
Vinoth <r.vinoth@live.com>
2024-01-05 18:51:55 +0100

    Added Bing Map Plugin Demo

---
+++ SKSampleCSharp/BingMapPluginDemo/Config.cs
@@ -0,0 +1,28 @@
+﻿namespace BingMapPluginDemo
+{
+    /// <summary>
+    /// Configurations for Azure OpenAI Chat Completion service
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// Azure OpenAI deployment name
+        /// </summary>
+        public static string DeploymentOrModelId => "gpt4model";
+
+        /// <summary>
+        /// Azure OpenAI endpoint
+        /// </summary>
+        public static string Endpoint => "";
+
+        /// <summary>
+        /// Azure OpenAI Key 
+        /// </summary>
+        public static string ApiKey => "";
+
+      /// <summary>
+      /// Bing Map Key ( Create a Key in Azure Portal )
+      /// </summary>
+        public static string BingMapKey => "";
+    }
+}
---
+++ SKSampleCSharp/BingMapPluginDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using AzureAI.Community.Microsoft.Semantic.Kernel.PlugIn.Web.BingMap.SuggestionAddress;
+using Microsoft.SemanticKernel;
+
+namespace BingMapPluginDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Bing Map Plugin in Semantic Kernel - Demo");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            var kernel = CreateKernelBuilder();
+
+            //Create Kernel function
+            var searchKernelFunction = kernel.Plugins.GetFunction(nameof(SuggestionAddressPlugin), "SearchSuggestion");
+
+            //Call the function
+            var result = await kernel.InvokeAsync(searchKernelFunction, new KernelArguments()
+            {
+                { "Suggestion", "Coffee" },
+                { "city", "Thanjavur" }
+            });
+
+            Console.WriteLine(result.GetValue<string>());
+            Console.Read();
+        }
+
+        private static Kernel CreateKernelBuilder()
+        {
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var builder = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(Config.DeploymentOrModelId, Config.Endpoint, Config.ApiKey);
+
+            //Add the Bing Map plugin
+            SuggestionAddressPlugin suggestionAddressPlugin = new(Config.BingMapKey);
+
+            builder.Plugins.AddFromObject(suggestionAddressPlugin);
+
+            return builder.Build()!;
+        }
+    }
+}
