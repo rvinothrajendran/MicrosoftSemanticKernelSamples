a737ec5497cca33a4311835cfb64284072dc23e2
Vinoth <r.vinoth@live.com>
2024-07-07 06:46:10 +0200

    function calling helper

---
+++ SKSampleCSharp/FunctionCallingHelper/Config.cs
@@ -0,0 +1,28 @@
+﻿namespace FunctionCallingHelper
+{
+    /// <summary>
+    /// Configurations for Azure OpenAI Chat Completion service
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// Azure OpenAI deployment name
+        /// </summary>
+        public static string DeploymentOrModelId => "";
+
+        /// <summary>
+        /// Azure OpenAI endpoint
+        /// </summary>
+        public static string Endpoint => "";
+
+        /// <summary>
+        /// Azure OpenAI Key 
+        /// </summary>
+        public static string ApiKey => "";
+
+        /// <summary>
+        /// External API key for weather service
+        /// </summary>
+        public static string WeatherApiKey => "";
+    }
+}
