119f6f2f35d2a69a3d1fcf5d4e51cf70f85225c7
Vinoth <r.vinoth@live.com>
2024-09-19 09:51:29 +0200

    Added new function calling and AutoGen kernel plugin

---
+++ SKSampleCSharp/AutoGenKernelPlugin/Plugins/WeatherPlugin/ExternalWeatherPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace AutoGenKernelPlugin.Plugins.WeatherPlugin;
+
+public class ExternalWeatherPlugin
+{
+    private readonly string _apiKey = Config.WeatherApiKey;
+    private readonly HttpClient _httpClient = new();
+
+    public static string GetWeather => "GetWeatherAsync";
+
+    [KernelFunction, Description("get weather information based on the location")]
+    public string? GetWeatherAsync(string cityName)
+    {
+
+        var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={_apiKey}&q={cityName.ToString()}&aqi=no";
+
+        try
+        {
+            HttpResponseMessage response = _httpClient.GetAsync(apiUrl).Result;
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = response.Content.ReadAsStringAsync().Result;
+
+            return responseBody;
+        }
+        catch (HttpRequestException ex)
+        {
+            // Handle any exceptions here, such as network errors.
+            Console.WriteLine($"Error: {ex.Message}");
+            return null;
+        }
+    }
+}
---
+++ SKSampleCSharp/NewFunctionCalling/Program.cs
@@ -0,0 +1,49 @@
+﻿using AutoFunctionCalling.Plugins;
+using Microsoft.SemanticKernel;
+
+namespace AutoFunctionCalling
+{
+#pragma warning disable SKEXP0001
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var kernel = CreateKernelBuilder();
+
+            kernel.Plugins.AddFromType<CurrencyConverterPlugin>(nameof(CurrencyConverterPlugin));
+
+            KernelFunction euroToInrKernelFunction = kernel.Plugins.GetFunction(nameof(CurrencyConverterPlugin), "EuroToInr");
+            KernelFunction euroToDollarKernelFunction = kernel.Plugins.GetFunction(nameof(CurrencyConverterPlugin), "EuroToDollar");
+
+            // NewWay of Function Calling
+            PromptExecutionSettings settings = new PromptExecutionSettings()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(new List<KernelFunction>()
+                {
+                    euroToInrKernelFunction,
+                    euroToDollarKernelFunction
+                })
+            };
+
+            var result = await kernel.InvokePromptAsync("Convert 1 Euro to Dollar",new KernelArguments(settings));
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("\n\nPress any key to exit...");
+
+            Console.Read();
+        }
+
+
+        private static Kernel CreateKernelBuilder()
+        {
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var builder = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(Config.DeploymentOrModelId, Config.Endpoint, Config.ApiKey);
+
+            return builder.Build()!;
+        }
+    }
+}
