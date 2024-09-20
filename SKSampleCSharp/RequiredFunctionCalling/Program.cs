dd389951105dd36d07dad13bea32a60a4eeab13f
Vinoth <r.vinoth@live.com>
2024-09-20 07:19:36 +0200

    RequiredFunctionCalling

---
+++ SKSampleCSharp/RequiredFunctionCalling/Program.cs
@@ -0,0 +1,54 @@
+﻿using AutoFunctionCalling;
+using AzureAI.Community.SK.Connector.GitHub.Models.AzureOpenAI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using RequiredFunctionCalling.Plugins;
+
+namespace RequiredFunctionCalling
+{
+#pragma warning disable SKEXP0001
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var kernel = CreateKernelBuilder();
+
+            kernel.Plugins.AddFromType<ExternalWeatherPlugin>(nameof(ExternalWeatherPlugin));
+
+            KernelFunction weatherKernelFunction =
+                kernel.Plugins.GetFunction(nameof(ExternalWeatherPlugin), "GetWeatherAsync");
+
+
+            // NewWay of Function Calling
+            PromptExecutionSettings settings = new PromptExecutionSettings()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Required(new List<KernelFunction>()
+                {
+                    weatherKernelFunction
+                })
+            };
+
+            var result = await kernel.InvokePromptAsync("Provide the history of Chennai",new KernelArguments(settings));
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("\n\nPress any key to exit...");
+
+            Console.Read();
+        }
+
+
+        private static Kernel CreateKernelBuilder()
+        {
+            //Create a kernel builder and add the Azure OpenAI Chat Completion service
+            var builder = Kernel.CreateBuilder()
+                .AddGitHubAzureOpenAIChatCompletion(GitConfig.ModelId, GitConfig.Endpoint, GitConfig.GitHubToken);
+
+            builder.Services.AddSingleton<IPromptRenderFilter, PromptRenderFilter>();
+
+            return builder.Build()!;
+        }
+    }
+}
---
+++ SKSampleCSharp/RequiredFunctionCalling/PromptRenderFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace RequiredFunctionCalling;
+
+public class PromptRenderFilter : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+            
+        await next(context);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        context.RenderedPrompt += " include weather information also based on the city";
+
+        Console.WriteLine($"{context.RenderedPrompt}");
+    }
+}
