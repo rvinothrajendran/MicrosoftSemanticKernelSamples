c5f18419d571978624d4d68dca9f8bbd43ef830b
Vinoth <r.vinoth@live.com>
2024-10-18 04:13:02 +0200

    Added liquid Prompt Template

---
+++ SKSampleCSharp/LiquidPromptDemo/GitConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace LiquidPromptDemo
+{
+    /// <summary>
+    /// Configurations for Azure OpenAI Chat Completion service
+    /// </summary>
+    public static class GitConfig
+    {
+        /// <summary>
+        /// Azure OpenAI deployment name
+        /// </summary>
+        public static string ModelId => "gpt-4o";
+
+        /// <summary>
+        /// Azure OpenAI endpoint
+        /// </summary>
+        public static string Endpoint => "";
+
+        /// <summary>
+        /// Azure OpenAI Key 
+        /// </summary>
+        public static string GitHubToken => "";
+        
+
+    }
+}
